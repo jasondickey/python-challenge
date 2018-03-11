13e0710c7f9861cde81ae95161c1b1e5bbfbc92c
Jason Dickey <jd.glenn@ymail.com>
2018-03-10 19:35:24 -0800

    adding final version of pybank code

--- Pybank/main.py
+++ Pybank/main.py
@@ -2,13 +2,12 @@ import os
 import csv
 import numpy as n
 
-# define file paths
+# define paths for incoming and outgoing files
 file1 = os.path.join("budget_data_1.csv")
 file2 = os.path.join("budget_data_2.csv")
-
 output_path = os.path.join('new.txt')
 
-# read files
+# read incoming files
 csv_file1 = csv.reader(open(file1))
 csv_file2 = csv.reader(open(file2))
 
@@ -17,16 +16,22 @@ date = []
 rev = []
 total_rev = 0
 
-# create list of dates and sum total revenue for list 1
+# read through each file
 for row in csv_file1:
+
+    #insert column one into date list
     date.append(row[0])
+
+    #insert  column two into revenue list
     revenue = row[1]
 
-    # replace non-integer values with 0
+    # replace non-integer values with 0 for revenue list
     try:
         revenue = int(revenue)
     except ValueError:
         revenue = 0
+    
+    # add values in revenue list with each pass
     total_rev += revenue
     rev.append(revenue)
 
@@ -45,10 +50,11 @@ for row in csv_file2:
     total_rev += revenue
     rev.append(revenue)
 
-
+# set date and rev lists as arrays in order to use index
 d = n.array(date)
 r = n.array(rev)
 
+# identify min and max values of revenue, and locate their indexes
 idmx = n.where(r==r.max())[0]
 idmn = n.where(r==r.min())[0]
 
@@ -60,10 +66,9 @@ print("Average Revenue: $" + str(int(total_rev/len(date))))
 print("Greatest Increase in Revenue: " + str(d[int(idmx)]) + " ($" + str(r[int(idmx)]) + ")")  
 print("Greatest Decrease in Revenue: " + str(d[int(idmn)]) + " ($" + str(r[int(idmn)]) + ")") 
 
-
+# Initialize csv.writer and write file output
 with open(output_path, 'w', newline='') as csvfile:
-
-    # Initialize csv.writer
+    
     csvwriter = csv.writer(csvfile, delimiter=',')
     csvwriter.writerow(["Financial Analysis"])
     csvwriter.writerow(["------------------"])
