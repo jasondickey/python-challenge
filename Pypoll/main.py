255061c55b6f21bfd4ae80bb8c355fa9833c13d7
Jason Dickey <jd.glenn@ymail.com>
2018-03-12 18:04:20 -0700

    adding final version of pypoll code with comments!

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -7,6 +7,7 @@ file1 = os.path.join("election_data_1.csv")
 file2 = os.path.join("election_data_2.csv")
 output_path = os.path.join('new.txt')
 
+# build varibles to be used later
 Candidate = []
 Candidate_p = []
 Candidate_d = []
@@ -21,6 +22,7 @@ Otooley = 0
 Khan = 0
 Total = 0
 
+# establish candidate names -- this was done via multiple iterations after file was imported into a list. The lists distinct values were identified
 Candidate_1 = "Cordin"
 Candidate_2 = "Vestal"
 Candidate_3 = "Torres"
@@ -30,28 +32,31 @@ Candidate_6 = "Li"
 Candidate_7 = "O'Tooley"
 Candidate_8 = "Khan"
 
+# build varibles for %
 Candidate_1_p = 0
-Candidate_2_p = 0.00
-Candidate_3_p = 0.00
-Candidate_4_p = 0.00
-Candidate_5_p = 0.00
-Candidate_6_p = 0.00
-Candidate_7_p = 0.00
-Candidate_8_p = 0.00
+Candidate_2_p = 0
+Candidate_3_p = 0
+Candidate_4_p = 0
+Candidate_5_p = 0
+Candidate_6_p = 0
+Candidate_7_p = 0
+Candidate_8_p = 0
 
+# open both files and read them
 csv_file1 = csv.reader(open(file1))
 csv_file2 = csv.reader(open(file2))
 
-    # Loop through the data
+    # Loop through the data for file 1
 for row in csv_file1:
-        # If the wrestler's name in a row is equal to that which the user input, run the 'getPercentages()' function
+        # add each candidate row into a list
     Candidate.append(row[2])
     
-    # Loop through the data
+    # Loop through the data for file 2
 for row in csv_file2:
-        # If the wrestler's name in a row is equal to that which the user input, run the 'getPercentages()' function
+        # add candidates from 2nd file to list
     Candidate.append(row[2])        
 
+# find total number of votes for each candidate and calc total
 Cordin = Candidate.count(Candidate_1)
 Vestal = Candidate.count(Candidate_2)
 Torres = Candidate.count(Candidate_3)
@@ -62,6 +67,7 @@ Otooley = Candidate.count(Candidate_7)
 Khan = Candidate.count(Candidate_8)
 Total = Cordin + Vestal + Torres + Seth + Correy + Otooley + Khan
 
+# calculate the percentage for each candidate
 Candidate_1_p = Cordin/Total
 Candidate_2_p = Vestal/Total
 Candidate_3_p = Torres/Total
@@ -71,6 +77,7 @@ Candidate_6_p = Li/Total
 Candidate_7_p = Otooley/Total
 Candidate_8_p = Khan/Total
 
+# build list of all candidate percentages
 Candidate_p.append(Candidate_1_p)
 Candidate_p.append(Candidate_2_p)
 Candidate_p.append(Candidate_3_p)
@@ -80,6 +87,7 @@ Candidate_p.append(Candidate_6_p)
 Candidate_p.append(Candidate_7_p)
 Candidate_p.append(Candidate_8_p)
 
+# build list of call candidates
 Candidate_d.append(Candidate_1)
 Candidate_d.append(Candidate_2)
 Candidate_d.append(Candidate_3)
@@ -89,8 +97,10 @@ Candidate_d.append(Candidate_6)
 Candidate_d.append(Candidate_7)
 Candidate_d.append(Candidate_8)
 
+# create dictionary with percentage as key, and candidate name as value
 Candidate_scores = dict(zip(Candidate_p,Candidate_d))
 
+# print results of election
 print("Election Results")
 print("----------------")
 print("Total Votes: " + str(Total))
@@ -107,6 +117,7 @@ print("----------------")
 print("Winner: " + str(Candidate_scores[max(Candidate_p)]))
 
 
+# write results from election to text file
 with open(output_path, 'w', newline='') as csvfile:
 
     csvwriter = csv.writer(csvfile, delimiter=',')
